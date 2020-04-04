10af8b0738e1c220a0e7d3010ba094b54f08b80d
Syed Anas Ikhlas <syedanasikhlas2050@gmail.com>
2020-04-04 10:50:09 +0300

    added new about page and update table view

--- app.js
+++ app.js
@@ -139,7 +139,6 @@ for (var i = 0; i < formInput.length; i++) {
 
 // Main section
 
-
 const keyword = document.querySelector(".keyword");
 const generateButton = document.querySelector(".generate-button");
 // loading
@@ -153,74 +152,119 @@ const prefixTable = document.querySelector(".prefix-table");
 const suffixTable = document.querySelector(".suffix-table");
 const fakerTableFirstName = document.querySelector(".faker-table-first-name");
 const fakerTableLastName = document.querySelector(".faker-table-last-name");
-
+const table = document.querySelector("table");
 generateButton.addEventListener("click", function(event) {
   event.preventDefault();
+  document.querySelector(".verify-name-message").classList.add("show");
   if (keyword.value == "") {
+    // clear table data
+    // clearTable(table);
     document.querySelector(".msg").classList.toggle("red");
-    document.querySelector(".msg").innerHTML = "Please Enter a keyword!!!";
+    document.querySelector(".msg").innerHTML =
+      "Please Enter a keyword for better results!!!";
+    nameGen(faker.name.firstName());
   } else {
+    // clear table data
+    // clearTable(table);
     // removing any error message
     document.querySelector(".msg").classList.toggle("red");
     document.querySelector(".msg").innerHTML = "";
-    // Loading Animation
-    loading.classList.toggle("load");
-    // loading animation timeout
-   setTimeout(() => {
-      // Loading Animation
-      loading.classList.toggle("load");
-      // show names div
-      names.classList.add("show-grid");
-   }, 3000);
-  //  generate names fakersJs
-    for (let i = 0; i < 15; i++) {
-      // faker.name.findName();
-
-      // random words
-      var loremRow= loremTable.insertRow(1);
-      var loremCell = loremRow.insertCell(0);
-      loremCell.innerHTML = faker.lorem.word()+keyword.value;
-
-      // add random first name keywords
-      var fakerFirstNameRow = fakerTableFirstName.insertRow(1);
-      var fakerFirstNameCell = fakerFirstNameRow.insertCell(0);
-      fakerFirstNameCell.innerHTML = faker.name.firstName()+keyword.value;
-      
-      
-      // add random last name keywords
-      var fakerLastNameRow = fakerTableLastName.insertRow(1);
-      var fakerLastNameCell = fakerLastNameRow.insertCell(0);
-      fakerLastNameCell.innerHTML = keyword.value+faker.name.lastName();
-      
-      // add random alpha num keywords
-      var alphaNumTableRow = alphaNumTable.insertRow(1);
-      var alphaNumTableCell = alphaNumTableRow.insertCell(0);
-      alphaNumTableCell.innerHTML = keyword.value+faker.random.alphaNumeric();
-      
-      // add random num keywords
-      var numTableRow = numTable.insertRow(1);
-      var numTableCell = numTableRow.insertCell(0);
-      numTableCell.innerHTML = keyword.value+faker.random.number();
-      
-      // add prefix to keywords
-      var prefixTableRow = prefixTable.insertRow(1);
-      var prefixTableCell = prefixTableRow.insertCell(0);
-      prefixTableCell.innerHTML = faker.name.prefix()+"."+keyword.value+faker.random.word();
-      
-      // add suffix to keywords
-      var suffixTableRow = suffixTable.insertRow(1);
-      var suffixTableCell = suffixTableRow.insertCell(0);
-      suffixTableCell.innerHTML = keyword.value+faker.name.suffix()
-
-      
-
-      
-    }
+    nameGen(keyword.value);
   }
 });
 
+function nameGen(keyword) {
+  // Loading Animation
+  loading.classList.toggle("load");
+  // loading animation timeout
+  setTimeout(() => {
+    // Loading Animation
+    loading.classList.toggle("load");
+    // show names div
+    names.classList.add("show-grid");
+  }, 3000);
+
+  // Table headings
+  addContentToTable(loremTable, 0, 0, "Random words", "bold");
+  // fakerTableFirstName
+  addContentToTable(fakerTableFirstName, 0, 0, "Random keywords", "bold");
+  // fakerLastNameRow
+  addContentToTable(fakerTableLastName, 0, 0, "Random keywords", "bold");
+  // alphaNumTable
+  addContentToTable(alphaNumTable, 0, 0, "AlphaNumeric", "bold");
+  // num
+  addContentToTable(numTable, 0, 0, "Number", "bold");
+  // prefixTable
+  addContentToTable(prefixTable, 0, 0, "Prefix", "bold");
+  // alphaNumTable
+  addContentToTable(suffixTable, 0, 0, "Suffix", "bold");
+
+  //  generate names fakersJs
+  for (let i = 0; i < 15; i++) {
+    // faker.name.findName()
+    // random words
+    var loremRow = loremTable.insertRow(1);
+    var loremCell = loremRow.insertCell(0);
+    const lorem = faker.lorem.word() + keyword;
+    loremCell.innerHTML = `<a href="https://tiktok.com/@${lorem}" target="_blank">${lorem}</a>`;
+
+    // add random first name keywords
+    var fakerFirstNameRow = fakerTableFirstName.insertRow(1);
+    var fakerFirstNameCell = fakerFirstNameRow.insertCell(0);
+    const fakerFirstNameC = faker.name.firstName() + keyword;
+    fakerFirstNameCell.innerHTML = `<a href="https://tiktok.com/@${fakerFirstNameC}" target="_blank">${fakerFirstNameC}</a>`;
+
+    // add random last name keywords
+    var fakerLastNameRow = fakerTableLastName.insertRow(1);
+    var fakerLastNameCell = fakerLastNameRow.insertCell(0);
+    const fakerLastNameC = keyword + faker.name.lastName();
+    fakerLastNameCell.innerHTML = `<a href="https://tiktok.com/@${fakerLastNameC}" target="_blank">${fakerLastNameC}</a>`;
+
+    // add random alpha num keywords
+    var alphaNumTableRow = alphaNumTable.insertRow(1);
+    var alphaNumTableCell = alphaNumTableRow.insertCell(0);
+    const alphaNumC = keyword + faker.random.alphaNumeric();
+    alphaNumTableCell.innerHTML = `<a href="https://tiktok.com/@${alphaNumC}" target="_blank">${alphaNumC}</a>`;
+
+    // add random num keywords
+    var numTableRow = numTable.insertRow(1);
+    var numTableCell = numTableRow.insertCell(0);
+    const numTableC = keyword + faker.random.number();
+    numTableCell.innerHTML = `<a href="https://tiktok.com/@${numTableC}" target="_blank">${numTableC}</a>`;
+
+    // add prefix to keywords
+    var prefixTableRow = prefixTable.insertRow(1);
+    var prefixTableCell = prefixTableRow.insertCell(0);
+    const prefixTableC =
+      faker.name.prefix() + "." + keyword + faker.random.word();
+    prefixTableCell.innerHTML = `<a href="https://tiktok.com/@${prefixTableC}" target="_blank">${prefixTableC}</a>`;
+
+    // add suffix to keywords
+    var suffixTableRow = suffixTable.insertRow(1);
+    var suffixTableCell = suffixTableRow.insertCell(0);
+    suffixTableC = keyword + faker.name.suffix();
+    suffixTableCell.innerHTML = `<a href="https://tiktok.com/@${suffixTableC}" target="_blank">${suffixTableC}</a>`;
+  }
+}
+
 // copy event
-document.addEventListener('copy', (event) => {
-  console.log(event)
-  // window.location.replace(`https://tiktok.com/@${document.execCommand('paste')}`);
-})
+document.addEventListener("copy", event => {
+  console.log(event);
+  createModal(
+    `Checkout <a href="https://youtubenamegenerator.ml" target="_blank">YouTube</a> name generator.`
+  );
+  // alert(
+  //   `Checkout <a href="https://youtubenamegenerator.ml" target="_blank>YouTube</a> name generator.`
+  // );
+});
+
+function addContentToTable(table, row, cell, text, weight) {
+  var rowHead = table.insertRow(row);
+  var tableCellHead = rowHead.insertCell(cell);
+  tableCellHead.innerHTML = text;
+  tableCellHead.style.fontWeight = weight;
+}
+
+function clearTable(table) {
+  table.innerHTML = "";
+}
